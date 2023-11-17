d150b1cc5737c2a51f8b76d6c72845567c237a1f
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-17 15:16:25 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 const Header = () => {
     return (
         <section>
-
-
+            <nav>
+                    <a href="#">JOHN DOE</a>
+                    <div>
+                        <ul>
+                            <li>
+                                <a class="active" aria-current="page" href="#">ACCUEIL</a>
+                            </li>
+                            <li>
+                                <a href="#">SERVICES</a>
+                            </li>
+                            <li>
+                                <a  href="#">RÉALISATIONS</a>
+                            </li>
+                            <li>
+                                <a href="#">BLOG</a>
+                            </li>
+                            <li>
+                                <a href="#">ME CONTACTER</a>
+                            </li>
+                        </ul>
+                    </div>
+            
+            </nav>
         </section>
     )
 }
