3d1ec112d593f62ba4270623c938db888b76adc1
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-18 16:19:43 +0100

    legal section added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Header from './components/Header';
 import Home from './views/Home';
 import Realisations from './views/Realisations';
 import Blog from './views/Blog';
+import Legal from './views/Legal';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Blog />
+      <Legal />
     </div>
   );
 }
