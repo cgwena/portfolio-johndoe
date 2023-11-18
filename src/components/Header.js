a4d1db40daafc3f7fd2ac6cab98b53bbdeee51ff
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-18 15:43:54 +0100

    nav style added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,34 @@
 const Header = () => {
     return (
         <section>
-             <nav>
-                    <a href="#">JOHN DOE</a>
-                    <div>
-                        <ul>
-                            <li>
-                                <a className="active" aria-current="page" href="#">ACCUEIL</a>
+            <nav className="navbar navbar-expand-lg bg-dark fixed-top">
+                <div className="container-fluid ">
+                    <a className="navbar-brand text-bg-dark p-3 text-uppercase" href="#">John Doe</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ">
+                            <li className="nav-item">
+                                <a className="nav-link active text-bg-dark p-3 text-uppercase" aria-current="page" href="#">Accueil</a>
                             </li>
-                            <li>
-                                <a href="#">SERVICES</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Services</a>
                             </li>
-                            <li>
-                                <a  href="#">RÉALISATIONS</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Réalisations</a>
                             </li>
-                            <li>
-                                <a href="#">BLOG</a>
+                            <li className="nav-item ">
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Blog</a>
                             </li>
-                            <li>
-                                <a href="#">ME CONTACTER</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Me contacter</a>
                             </li>
                         </ul>
                     </div>
+                </div>
             </nav>
+
         </section>
     )
 }
