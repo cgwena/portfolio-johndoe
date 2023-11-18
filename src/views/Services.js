fa4bd0dd110e1a2233f250a8f1e62143d4b81f4b
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-18 16:22:33 +0100

    services section added

--- src/views/Services.js
+++ src/views/Services.js
@@ -1,8 +1,12 @@
+import Banner from "../components/Banner"
+
 const Services = () => {
     return (
         <section>
-
-
+            <Banner />
+            <h2 className="text-uppercase text-center pt-5">mes offres de services</h2>
+            <p className="text-center">Voici les prestations sur lesquelles je peux intervenir.</p>
+            <div className="bordure"></div>
         </section>
     )
 }
