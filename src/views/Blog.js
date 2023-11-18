5a00b90dd8c8f0e4ac5e1e0541e148802e595752
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-18 16:16:31 +0100

    blog section added

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,8 +1,12 @@
+import Banner from "../components/Banner"
+
 const Blog = () => {
     return (
         <section>
-
-
+            <Banner />
+            <h2 className="text-uppercase text-center pt-5">blog</h2>
+            <p className="text-center">Retrouvez ici quelques articles sur le développement web.</p>
+            <div className="bordure"></div>
         </section>
     )
 }
