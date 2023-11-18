9a576f905d469273ce449e54f964c5057259da76
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-18 16:35:14 +0100

    router added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Home from './views/Home';
@@ -10,8 +11,15 @@ import Contact from './views/Contact';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Contact />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Realisations />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/legal" element={<Legal />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,26 @@ const Header = () => {
         <section>
             <nav className="navbar navbar-expand-lg bg-dark fixed-top">
                 <div className="container-fluid ">
-                    <a className="navbar-brand text-bg-dark p-3 text-uppercase" href="#">John Doe</a>
+                    <a className="navbar-brand text-bg-dark p-3 text-uppercase" href="#/">John Doe</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ">
                             <li className="nav-item">
-                                <a className="nav-link active text-bg-dark p-3 text-uppercase" aria-current="page" href="#">Accueil</a>
+                                <a className="nav-link active text-bg-dark p-3 text-uppercase" aria-current="page" href="/">Accueil</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Services</a>
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="/services">Services</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Réalisations</a>
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="/portfolio">Réalisations</a>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Blog</a>
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="/blog">Blog</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="#">Me contacter</a>
+                                <a className="nav-link text-bg-dark p-3 text-uppercase" href="/contact">Me contacter</a>
                             </li>
                         </ul>
                     </div>
