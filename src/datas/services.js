4f164743d685834d6e15e03c4ff0ddefc531f632
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-19 18:11:00 +0100

    service card component added

---
+++ src/datas/services.js
@@ -0,0 +1,22 @@
+export const services = [
+    {
+        "id": 1,
+        "title" : "UX Design",
+        "text" : "L'UX design est une méthode conception centrée sur l'utilisateur. Son but est d'offrir une expérience de navigation optimale à l'utilisateur.",
+        "icon": "fa-solid fa-desktop"
+    },
+
+    {
+        "id": 2,
+        "title" : "Développement web",
+        "text" : "Le développement de sites web repose sur l'utilisation des langages HTML, CSS, Javascript et PHP.",
+        "icon": "fa-solid fa-file-code"
+    },
+
+    {
+        "id": 3,
+        "title" : "Référencement",
+        "text" : "Le référencement naturel d'un site, aussi appelé SEO, consiste à mettre des techniques en oeuvre pour améliorer sa position dans les moteurs de recherche.",
+        "icon" : "fa-solid fa-magnifying-glass-dollar"
+    }
+]
--- src/views/Services.js
+++ src/views/Services.js
@@ -1,14 +1,22 @@
 import Banner from "../components/Banner"
 import ServiceCard from "../components/Service-card"
+import {services} from '../datas/services'
 
 const Services = () => {
+
+    const service = services.map(service => {
+        return <ServiceCard title={service.title} text={service.text} icon={service.icon} />
+    })
+
     return (
         <section>
             <Banner />
             <h2 className="text-uppercase text-center pt-5">mes offres de services</h2>
             <p className="text-center">Voici les prestations sur lesquelles je peux intervenir.</p>
             <div className="bordure"></div>
-            <ServiceCard />
+            <div className='row m-4'>
+                {service}
+            </div>
         </section>
     )
 }
