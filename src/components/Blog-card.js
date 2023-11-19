ad733f50f3857069e47549c8514d7059809edde5
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-19 17:21:52 +0100

    blog-card-components added

--- src/components/Blog-card.js
+++ src/components/Blog-card.js
@@ -1,20 +1,16 @@
-import code from '../images/code-1076533_1280.jpg'
 
-const BlogCard = () => {
+
+const BlogCard = (props) => {
     return (
-        <div className='row'>
-            <div class="card col-lg-4 col-md-6">
-                <img src={code} class="card-img-top" alt="du code html" />
-                <div class="card-body">
-                    <h3 class="card-title">Coder son site en HTML/CSS</h3>
-                    <p class="card-text">Lorem ipsum</p>
-
-                    <a href="#" class="btn btn-primary">Lire la suite</a>
-
-                </div>
-                <div class="card-footer text-muted">
-                    Publié le 22 août 2023
-                </div>
+        <div class="card col-lg-4 col-md-6 mb-3">
+            <img src={props.img} class="card-img-top" alt={props.alt} />
+            <div class="card-body">
+                <h3 class="card-title">{props.title}</h3>
+                <p class="card-text">{props.text}</p>
+                <a href="#" class="btn btn-primary">Lire la suite</a>
+            </div>
+            <div class="card-footer text-muted">
+                <p>Publié le {props.date}</p>
             </div>
         </div>
     )
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,14 +1,21 @@
 import Banner from "../components/Banner"
 import BlogCard from "../components/Blog-card"
+import { blogArticles } from '../datas/blogArticles.js'
 
 const Blog = () => {
+    const blogArticle = blogArticles.map(article => {
+        console.log(article.img)
+        return <BlogCard img={article.img} title={article.title} text={article.text} date={article.date} alt={article.alt} />
+    })
     return (
         <section>
             <Banner />
             <h2 className="text-uppercase text-center pt-5">blog</h2>
             <p className="text-center">Retrouvez ici quelques articles sur le développement web.</p>
             <div className="bordure"></div>
-            <BlogCard />
+            <div className='row m-4'>
+                {blogArticle}
+            </div>
         </section>
     )
 }
