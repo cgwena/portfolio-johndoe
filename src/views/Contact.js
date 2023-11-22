3b2cd8fdb7caf15772a98dc8655b3ef0880bbb31
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-22 12:18:01 +0100

    link added on hero - bg image added in contact

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,18 +1,29 @@
-import Banner from "../components/Banner"
 import Form from "../components/Form"
 import Coordonnees from "../components/Coordonnees"
 
 const Contact = () => {
     return (
-        <section>
-            <Banner />
-            <h2 className="text-uppercase text-center pt-5">Me contacter</h2>
-            <p className="text-center">Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
-            <div className="bordure"></div>
-            <div class='row'>
-                <Form />
-                <Coordonnees />
+
+        <section className="contact mt-5">
+            <div className="contact__img">
+                <div className="contact__overlay"></div>
+                <div className="my-5">
+                    <div class='m-5 p-5 bg-white rounded'>
+                        <div class='d-flex flex-column'>
+                            <h2 className="text-uppercase text-center pt-5">Me contacter</h2>
+                            <p className="text-center">Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
+                            <div className="bordure"></div>
+                        </div>
+                        <div class='row'>
+                            <Form />
+                            <Coordonnees />
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
+
         </section>
     )
 }
