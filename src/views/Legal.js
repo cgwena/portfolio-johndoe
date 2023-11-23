ae173c7a2d240479dd258fb31ac4e837c6699b43
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-23 09:29:22 +0100

    seo

--- src/views/Legal.js
+++ src/views/Legal.js
@@ -1,9 +1,13 @@
 import Banner from "../components/Banner"
 import LegalAccordion from "../components/LegalAccordion"
+import MetaTags from 'react-meta-tags';
 
 const Legal = () => {
     return (
         <section>
+            <MetaTags>
+                <meta name="robots" content="noindex" />
+            </MetaTags>
             <Banner />
             <h2 className="text-uppercase text-center pt-5">mentions légales</h2>
             <div className="bordure"></div>
