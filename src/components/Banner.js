15bf4676c890b38d86b5536b921be5bc71d6912c
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-24 14:41:32 +0100

    changes W3CValidator

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import background from '../images/background-2672565_1280.jpg'
 const Banner = () => {
     return (
         <section>
-            <img className='banner__img img-fluid' src={background}></img>
+            <img className='banner__img img-fluid' alt='des traces de rayons lumineux blancs sur un fond bleu' src={background}></img>
 
         </section>
     )
