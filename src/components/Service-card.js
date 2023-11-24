55e6ee0aab1dbbbc9fd248babe3c43077dceb746
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-24 12:53:36 +0100

    style fix

--- src/components/Service-card.js
+++ src/components/Service-card.js
@@ -2,7 +2,7 @@ const ServiceCard = (props) => {
     return (
         <li className=" col-lg-4 col-md-4 text-center mb-3" >
             <div className="card service-card">
-                <i className={`${props.icon} icon p-3`} key={props.id}></i>
+                <i className={`${props.icon} icon p-5`} key={props.id}></i>
                 <h3 className="card-title text-uppercase p-3">{props.title}</h3>
                 <h5 className="card-text p-3">{props.text}</h5>
             </div>
